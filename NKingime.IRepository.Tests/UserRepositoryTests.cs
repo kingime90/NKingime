65f733c443637030b8246c0fba8653f7e0abf086
aiming.wang <aiming.wang@fmchexian.com>
2017-08-09 11:11:45 +0800

    单元测试

---
+++ NKingime.IRepository.Tests/UserRepositoryTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace NKingime.IRepository.Tests
+{
+
+    /// <summary>
+    /// 用户数据仓储单元测试
+    /// </summary>
+    [TestFixture]
+    public class UserRepositoryTests
+    {
+        /// <summary>
+        /// 初始化
+        /// </summary>
+        [SetUp]
+        public static void Initialize()
+        {
+            
+        }
+
+        /// <summary>
+        /// 测试保存
+        /// </summary>
+        [Test]
+        public void SaveTest()
+        {
+
+        }
+    }
+}
